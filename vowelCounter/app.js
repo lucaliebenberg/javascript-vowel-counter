d5efd93cabbb3b0dd215f4c2fac08ea6ab262bf6
Luca Liebenberg <liebenbergluca@gmail.com>
2022-07-30 09:06:17 +0200

    initial commit

---
+++ vowelCounter/app.js
@@ -0,0 +1,15 @@
+const textBox = document.querySelector('.wrapper textarea'),
+    checkBtn = document.querySelector('#check-btn'),
+    vowelCountShow = document.querySelector('#vowel-count');
+vowelCountShow.innerHTML = 0;
+
+checkBtn.addEventListener('click', () => {
+    let count = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    for(let letter of textBox.nodeValue.toLocaleLowerCase()){
+        if(vowels.includes(letter)){
+            count++;
+        }
+    }
+    vowelCountShow.innerHTML = count;
+});
